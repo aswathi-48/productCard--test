2776469b445d61cc455a9e8c9e9ae6b03d747fb2
happle joseph <happlejoseph66@gmail.com>
2026-05-14 23:09:47 +0530

    add products dynmicaly now

--- js/displayProducts.js
+++ js/displayProducts.js
@@ -2,22 +2,35 @@
 
 let productList = document.getElementById('productsList');
 
-let = allProducts = JSON.parse(localStorage.getItem('products')) || [];
+let allProducts = JSON.parse(localStorage.getItem('products')) || [];
 
 function displayProducts() {
     productList.innerHTML = "";
 
     allProducts.forEach(product => {
-        productList.innerHTML = 
+        productList.innerHTML += 
             `
             <div class="card">
-                <img src="${product.image}" alt="">
-                <h3 ${product.name}></h3>
-                <p ${product.price}></p>
-                <p ${product.category}></p>
-                <
+                <div class="image-section">
+                    <img src="${product.image}" alt="">
+                </div>
+                
+                <div class="content-section">
+                    <h3>${product.name}</h3>
+                    <p>${product.price}</p>
+                    <p>${product.category}</p>
+                    <p>${product.quantity}</p>
+                    <p>${product.description}</p>
+                </div>
+                
+                <div class="actions">
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div>    
             </div>
 
             `
+    displayProducts();
+            
     });
 }
