e41cce90492342e46781bda9de0794e2a584c84f
happlejoseph <vyshnavanil18753@gamil.com>
2026-05-18 12:04:39 +0530

    changes

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@ let addBtn = document.getElementById('addBtn');
 let viewBtn = document.getElementById('viewBtn');
 
 addBtn.addEventListener('click' ,()=> {
-    window.location.href = '/pages/addProducts.html'
+    window.location.href = './pages/addProducts.html'
 });
 
 viewBtn.addEventListener('click', ()=> {
-    window.location.href = '/pages/displayProducts.html'
+    window.location.href = './pages/displayProducts.html'
 });
