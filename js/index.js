e229773337770997527ad6279367ac4cacde363b
aswathi-48 <aswathik482@gmail.com>
2026-05-18 13:08:53 +0530

    changes

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ let addBtn = document.getElementById('addBtn');
 let viewBtn = document.getElementById('viewBtn');
 
 addBtn.addEventListener('click' , ()=> {
-    window.location.href ='./pages/addProduct.html'
+    window.location.href ='./Product-Management-System/pages/addProduct.html'
 });
 
 viewBtn.addEventListener('click', ()=> {
-    window.location.href ='./pages/displayProducts.html'
+    window.location.href ='./Product-Management-System/pages/displayProducts.html'
 });
