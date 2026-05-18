9774fb97350e847518d1eefadf35b72180aa6a48
aswathi-48 <aswathik482@gmail.com>
2026-05-18 13:11:45 +0530

    change

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ let addBtn = document.getElementById('addBtn');
 let viewBtn = document.getElementById('viewBtn');
 
 addBtn.addEventListener('click' , ()=> {
-    window.location.href ='../pages/addProduct.html'
+    window.location.href ='/pages/addProduct.html'
 });
 
 viewBtn.addEventListener('click', ()=> {
